507043dc6b185a4f0ea921edf5725d5818e06e31
Carou5el <garrydeepcool@outlook.com>
2021-02-02 21:32:44 +0500

    All tasks is solved.

--- src/geekbrains/lesson3Practical/Main.java
+++ src/geekbrains/lesson3Practical/Main.java
@@ -1,9 +1,21 @@
 package geekbrains.lesson3Practical;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+    static Random rndGenerator = new Random();
+    static Scanner scanUser = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Task1();
+        Task2();
+
+    }
+
     /***
      * Задание 1.
      *
@@ -14,12 +26,9 @@ public class Main {
      * После победы или проигрыша выводится запрос – «Повторить игру еще раз?
      * 1 – да / 0 – нет»(1 – повторить, 0 – нет).
      *
-     * @param args
      */
-    public static void main(String[] args) {
+    private static void Task1() {
 
-        Scanner scanUser = new Scanner(System.in);
-        Random rndGenerator = new Random();
         int guessingNumber = 0;
 
         do {
@@ -55,6 +64,109 @@ public class Main {
             System.out.printf("\nПопробуете ещё раз?\n(1 - да / 0 - нет)  ");
 
         } while (scanUser.nextInt() != 0);
+    }
+
+    /**
+     * Задание 2.
+     *
+     * При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+     * сравнивает его с загаданным словом и сообщает, правильно ли ответил пользователь.
+     * Если слово не угадано, компьютер показывает буквы, которые стоят на своих местах.
+     */
+    private static void Task2() {
+
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
+                        "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango",
+                        "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple",
+                        "pumpkin", "potato"};
+
+        int lengthTestArray = words.length;
+        int indexOfResultWord = rndGenerator.nextInt(lengthTestArray);
+
+        String resultWord = words[indexOfResultWord];
+        String userAnswer;
+
+        System.out.printf("Было загадано слово из представленных ниже. Отгадайте его.\n");
+        OutputWordsArrayToConsole(words);
+
+        do {
+
+            // Ввод варианта пользователя.
+            System.out.printf("Ваш ответ (выход - q): ");
+            userAnswer = scanUser.nextLine();
+
+            // Обработка выигрыша.
+            if (userAnswer.equals(resultWord))   {
+                System.out.printf("==============================\n");
+                System.out.printf("\tВы выиграли!\n\tКонец игры.\n");
+                System.out.printf("==============================\n");
+                break;
+            }
+
+            // Обработка, вывод посимвольных совпадений + маска.
+            CheckWords(userAnswer, resultWord);
+
+            System.out.println();
+            System.out.printf("Попробуйте ещё раз...\n");
+
+            OutputWordsArrayToConsole(words);
+
+        }   while(!userAnswer.equals("q"));
+
+    }
+
+    /**
+     * Вывод содержимого массива из Задания 2 на консоль.
+     */
+    private static void OutputWordsArrayToConsole (String[] words)  {
+
+        int j = 0;
+
+        System.out.println();
+
+        for (String str : words)    {
+
+            System.out.printf("%s, ", str);
+            j++;
+
+            // Формирование переносов после каждого 5-го элемента.
+            if ((j % 5) == 0)   {
+                System.out.printf("\n");
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * Посимвольное сравнение параметра userWord по отношению resultWord.
+     * Совпадающие символы - выводятся в консоль.
+     * Остальные символы маскируются '#'.
+     * Всего - 15 позиций символов.
+     */
+    private static void CheckWords (String userWord, String resultWord)    {
+
+        // Длина замаскированного "слова".
+        final int maskedCharsCount = 15;
+
+        // Маркер на начальный символ.
+        int markerBeginWord = 0;
+
+        // Формирование 15-символьной маски.
+        for (int i = 0; i < maskedCharsCount; i++)    {
+
+            if (userWord.charAt(markerBeginWord) == resultWord.charAt(markerBeginWord)) {
+                // Вывод совпадающего символа.
+                System.out.printf("%c", userWord.charAt(markerBeginWord));
+                // Сдвиг маркера на 1.
+                markerBeginWord++;
+                continue;
+
+            } else {
+                System.out.printf("#");
+            }
+        }
+
+        System.out.printf("\n");
 
     }
 }
